b67d94d76a045d70c27349d27d4d4a05a0d9bd12
ArtiomLukashevich <artiomlukashevich@gmail.com>
2022-08-01 14:16:25 +0300

    crate project

---
+++ pages/api/data/houseItems.js
@@ -0,0 +1,32 @@
+;[
+	{
+		id: 1,
+		description: { Bedrooms: 1, Bath: 1, roomArea: '535 sq ft' },
+		price: 100.0,
+	},
+	{
+		id: 2,
+		description: { Bedrooms: 2, Bath: 3, roomArea: '1000 sq ft' },
+		price: 390.0,
+	},
+	{
+		id: 3,
+		description: { Bedrooms: 1, Bath: 1, roomArea: '535 sq ft' },
+		price: 143.0,
+	},
+	{
+		id: 4,
+		description: { Bedrooms: 1, Bath: 1, roomArea: '535 sq ft' },
+		price: 211.2,
+	},
+	{
+		id: 5,
+		description: { Bedrooms: 1, Bath: 1, roomArea: '535 sq ft' },
+		price: 132.2,
+	},
+	{
+		id: 6,
+		description: { Bedrooms: 1, Bath: 1, roomArea: '535 sq ft' },
+		price: 190.4,
+	},
+]
